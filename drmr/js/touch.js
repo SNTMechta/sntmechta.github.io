dc5ebd44bf9fdf87aa561968205fe066863a3db1
❄️МЕЧТА❄️ <SNTMechta@users.noreply.github.com>
2017-02-14 12:56:04 +0400

    Для заглушки-форума

---
+++ drmr/js/touch.js
@@ -0,0 +1,1 @@
+// https://github.com/MicrosoftEdge/Demos/tree/master/letitsnowTouch = (function () {        "use strict";function preventEvents(evtObj) { /*  if (evtObj.preventDefault)   evtObj.preventDefault();  if (evtObj.preventManipulation)   evtObj.preventManipulation();  if (evtObj.preventMouseEvent)   evtObj.preventMouseEvent(); */    }return {"preventEvents": preventEvents}})();
